faac4a22223c1d8510ea7fc61750f1bd01c7c437
Robert Brinton <robertpbrinton@gmail.com>
2015-01-31 11:20:12 -0700

    commiting babies first angular

---
+++ js/mainCtrl.js
@@ -0,0 +1,8 @@
+var app = angular.module("friendsList");
+
+app.controller("mainCtrl", function($scope) {
+	$scope.friends = ["Jeff", "Tonio", "Todd", "Preston"];
+	$scope.addFriend = function() {
+
+	}
+});
